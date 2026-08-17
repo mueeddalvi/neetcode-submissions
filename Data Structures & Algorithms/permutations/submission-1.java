c84bb12c167d2881d50b99f54a8c5a75169c2005
Abdul Dalvi <54925573+mueeddalvi@users.noreply.github.com>
2026-08-17 18:59:59 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    List<List<Integer>> res;
+    public List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
+        int len = nums.length;
+        Set<Integer> set = new HashSet<>();
+
+        compute(nums, new ArrayList<>(), new HashSet<>());
+
+        return res;
+    }
+
+    public void compute(int nums[], List<Integer> curr, Set<Integer> set) {
+        if (curr.size() == nums.length) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(nums[i])) {
+                curr.add(nums[i]);
+                set.add(nums[i]);
+                compute(nums, curr, set);
+                curr.remove(curr.size() - 1);
+                set.remove(nums[i]);
+            }
+        }
+    }
+}
